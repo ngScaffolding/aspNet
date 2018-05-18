1768176f0412294d3cbb6986b083bca2c9c3c85f
dbaines30 <dbaines1@hotmail.com>
2018-05-18 07:54:04 +0100

    Dashboard Models

---
+++ ngScaffolding-models/Models/DashboardModels/DashboardModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ngScaffolding.Models
+{
+    public class DashboardModel
+    {
+        public string title { get; set; }
+        public bool? isReadOnly { get; set; }
+        public IEnumerable<WidgetModel> widgets { get; set; }
+
+        public IEnumerable<string> userIds { get; set; }
+        public IEnumerable<string> groups { get; set; }
+    }
+}
---
+++ ngScaffolding-models/Models/DashboardModels/WidgetModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ngScaffolding.Models
+{
+    public class WidgetModel
+    {
+        public string title { get; set; }
+
+        public string configuredValues { get; set; }
+        public int? col { get; set; }
+        public int? row { get; set; }
+        public int? sizeX { get; set; }
+        public int? sizeY { get; set; }
+        public string style { get; set; }
+        public int? refreshInterval { get; set; }
+
+        public GridViewDetailModel gridViewDetail { get; set; }
+        public ChartDetailModel chartDetail { get; set; }
+        public HTMLContentModel htmlContent { get; set; }
+    }
+}
